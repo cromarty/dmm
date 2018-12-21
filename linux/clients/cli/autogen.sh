7ba725c4252bcc77da34fe8826ab0b3fe53ee037
Mike Ray <mike.ray@btinternet.com>
2018-12-21 00:26:46 +0000

    modified:   .gitignore 	new file:   linux/clients/.gitkeep 	new file:   linux/clients/cli/.gitignore 	new file:   linux/clients/cli/.gitkeep 	new file:   linux/clients/cli/AUTHORS 	new file:   linux/clients/cli/COPYING 	new file:   linux/clients/cli/ChangeLog 	new file:   linux/clients/cli/INSTALL 	new file:   linux/clients/cli/Makefile.am 	new file:   linux/clients/cli/Makefile.in 	new file:   linux/clients/cli/NEWS 	new file:   linux/clients/cli/README 	new file:   linux/clients/cli/TODO 	new file:   linux/clients/cli/aclocal.m4 	new file:   linux/clients/cli/autogen.sh 	new file:   linux/clients/cli/compile 	new file:   linux/clients/cli/config.h.in 	new file:   linux/clients/cli/configure 	new file:   linux/clients/cli/configure.ac 	new file:   linux/clients/cli/depcomp 	new file:   linux/clients/cli/fix-timestamps.sh 	new file:   linux/clients/cli/install-sh 	new file:   linux/clients/cli/missing 	new file:   linux/clients/cli/src/Makefile.am 	new file:   linux/clients/cli/src/Makefile.in 	new file:   linux/clients/cli/src/config.h.in 	new file:   linux/clients/cli/src/ut60a.c 	new file:   linux/lib/.gitignore 	modified:   linux/lib/Makefile.in 	modified:   linux/lib/aclocal.m4 	modified:   linux/lib/configure 	modified:   linux/lib/include/Makefile.in 	modified:   linux/lib/src/Makefile.in

---
+++ linux/clients/cli/autogen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+autoreconf --force --install --verbose "$srcdir"
---
+++ linux/clients/cli/src/ut60a.c
@@ -0,0 +1,18 @@
+#include "config.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+#include <uni_t_dmm/fs9721lp3.h>
+#include <uni_t_dmm/uni_t_dmm.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
+
